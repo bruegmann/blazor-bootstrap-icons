d7e0d0d7a79faa90469f803636aa2888a9ef731a
lgk-bsw <lgk@patorg.de>
2024-07-19 10:24:47 +0200

    chore: Add initial project files and configurations

---
+++ README.md
@@ -0,0 +1,41 @@
+# Blazor Bootstrap Icons
+
+Use the [Bootstrap Icons library](https://icons.bootstrap.com) as Blazor components. This solution is inspired by [React Bootstrap Icons](https://www.npmjs.com/package/react-bootstrap-icons).
+
+## Installation
+
+```
+dotnet add package Blazor.Bootstrap.Icons
+```
+
+## Usage
+
+```razor
+@using Blazor.Bootstrap.Icons.Components
+
+<BiArrowRight />
+```
+
+To make icons available globally in your project, you can add `@using Blazor.Bootstrap.Icons.Components` to your `_Imports.razor` file.
+
+The icon names are the `PascalCase` version of the original name. To avoid conflicts with any other components in your project, all component names start with the prefix `Bi`.
+
+## Parameters
+
+| Attribute | Default value                                  |
+| --------- | ---------------------------------------------- |
+| `Class`   | `"bi bi-"` + icon name (e.g. `bi-arrow-right`) |
+| `Color`   | `"currentColor"`\*                             |
+| `Size`    | `"1em"`                                        |
+
+You can add any other attribute like `title`, it will be attached to the SVG element.
+
+## More options
+
+Other ways to use Bootstrap Icons: [https://icons.getbootstrap.com/#usage](https://icons.getbootstrap.com/#usage)
+
+## License
+
+-   Blazor Bootstrap Icons are open-sourced ([MIT](https://github.com/bruegmann/blazor-bootstrap-icons/LICENSE))
+-   react-bootstrap-icons are open-sourced ([MIT](https://github.com/ismamz/react-bootstrap-icons/blob/master/LICENSE.md))
+-   Bootstrap Icons are open-sourced ([MIT](https://github.com/twbs/icons/blob/main/LICENSE))
---
+++ buildScripts/generate-blazor.js
@@ -0,0 +1,87 @@
+/**
+ * Takes SVG files from folders and transforms them to Blazor components.
+ */
+
+const path = require("path")
+const fs = require("fs")
+const upperCamelCase = require("uppercamelcase")
+
+const rootDir = path.join(__dirname, "..")
+
+const iconsDirs = [
+    path.join(rootDir, "node_modules", "bootstrap-icons", "icons")
+]
+
+const dir = path.join(rootDir, "Blazor.Bootstrap.Icons", "Components")
+
+if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir)
+}
+
+const prefix = "bi"
+
+let n = 0
+
+for (const iconsDir of iconsDirs) {
+    fs.readdirSync(iconsDir).forEach((file) => {
+        const svg = fs.readFileSync(`${iconsDir}/${file}`, "utf8")
+
+        if (file !== "house.svg" && file !== "house-fill.svg") {
+            return
+        }
+
+        console.log(`${iconsDir}/${file}`)
+
+        const fileName = file.split(".")[0]
+        const ComponentName = upperCamelCase(prefix + "_" + fileName)
+        const svgContent = svg
+            .replace(`<?xml version="1.0" encoding="UTF-8"?>`, "")
+            .replace(
+                `<?xml version="1.0" encoding="UTF-8" standalone="no"?>`,
+                ""
+            )
+            .replace(
+                `<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">`,
+                ""
+            )
+            .replace(/(<title>).*(<\/title>)/gm, "")
+            .replace(/(<title>).*(<\/title>)/gm, "")
+            .replace(/(<desc>).*(<\/desc>)/gm, "")
+            .replace(/<svg[^>]*>|<\/svg>/g, "")
+            .replace(/serif:id="(.*?)"/g, "")
+            .replace(/id="(.*?)"/g, "")
+            .replace(/<path  /g, "<path ")
+            .replace(/\t/g, "    ")
+            .trim()
+
+        const component = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" class="@Class" width="@Size" height="@Size"
+     fill="@Color" @attributes="AdditionalAttributes">
+    ${svgContent}
+</svg>
+
+@code {
+    [Parameter]
+    public string Class { get; set; } = "bi bi-${fileName}";
+
+    [Parameter]
+    public string Color { get; set; } = "currentColor";
+
+    [Parameter]
+    public string Size { get; set; } = "1em";
+
+    [Parameter(CaptureUnmatchedValues = true)]
+    public IDictionary<string, object>? AdditionalAttributes { get; set; }
+}`
+
+        fs.writeFileSync(
+            path.join(dir, `${ComponentName}.razor`),
+            component,
+            "utf-8"
+        )
+
+        console.log(`${ComponentName} was created.`)
+        n++
+    })
+}
+
+console.log(`\nSuccess! ${n} icons were created.`)
